cee14e7db1679e15c319b7b43a4edfb7fb479533
Rakapurbayu <134347269+Rakapurbayu@users.noreply.github.com>
2024-06-09 00:42:27 +0700

    Upload Scaptis Update For Dashboard Admin

---
+++ Scaptis Admin/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'KaraFuru Denim XL',
+        productNumber: 'JP1423561',
+        paymentStatus: 'Paid',
+        status: 'Sedang Dikirim'
+    },
+    {
+        productName: 'SSST Black Devise Backpack',
+        productNumber: 'JP2397245',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Best Friends Girl T-Shirt',
+        productNumber: 'JP1036002',
+        paymentStatus: 'Paid',
+        status: 'Sent'
+    },
+]
